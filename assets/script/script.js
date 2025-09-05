796aea68bdb711220c0b039faf984827a9c03959
Luigi9665 <ventriglia.luigi@hotmail.com>
2025-09-05 16:53:07 +0200

    add js per la manipolazione dell'svg

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,3 +9,27 @@ window.addEventListener("scroll", () => {
     nav.classList.remove("scrolled");
   }
 });
+
+const divSvg = document.querySelector(".svg");
+const svg = divSvg.getElementsByTagName("svg")[0];
+const allG = svg.querySelectorAll("g");
+
+function toggleOpacity() {
+  let randomNumber = Math.floor(Math.random() * allG.length);
+  const g = allG[randomNumber];
+
+  // Se ha clip-path o fill, esci subito
+  if (g.hasAttribute("clip-path") || g.hasAttribute("fill")) {
+    return; // esce dalla funzione senza fare nulla
+  }
+
+  //   altrimenti continua con le condizioni
+  if (g.getAttribute("opacity") === "0") {
+    g.setAttribute("opacity", "1");
+  } else {
+    g.setAttribute("opacity", "0");
+  }
+}
+
+// Cambia l'opacità ogni secondo
+setInterval(toggleOpacity, 10);
